5440c2c83006dc81d38a6359ca3634cc3e73bdd9
Erwan David <93119990+Tsouh@users.noreply.github.com>
2023-02-08 15:28:34 +0100

    Add files via upload
    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,73 @@
+console.log("reload");
+
+// 
+// var start=document.getElementById("homepage");
+// setTimeout(() => {
+//     start.style.display="none";
+//   }, 3000)
+var category=document.getElementsByClassName("category");
+var nbCategoryTotal=category.length;
+var categorySelected=0;
+var itemInCategory=category[categorySelected].getElementsByClassName("item");
+var nbItemInCategory=itemInCategory.length;
+var itemSelected=0;
+var toggleSetting=false
+// var test=category[0].getElementsByClassName("item")[0].classList.add("itemSelect");
+
+console.log('test');
+console.log();
+itemInCategory[itemSelected].style.border=" 5px solid white";
+console.log("category = "+categorySelected+"; item ="+itemSelected);
+
+onkeyup = function(evt){
+    switch(evt.keyCode){
+        case 37: // left
+            if(itemSelected-1>=0){
+                itemInCategory[itemSelected].style.border=" 5px solid rgb(174,90,33)";
+                itemSelected--;
+                itemInCategory[itemSelected].style.border=" 5px solid white";
+            }
+            break;
+        case 38: // up
+            if(categorySelected==0){
+                itemInCategory[itemSelected].style.border=" 5px solid rgb(174,90,33)";
+                document.getElementById("itemSetting").style.border=" 5px solid white";
+                toggleSetting=true;
+                categorySelected=-1;
+            }
+            if (categorySelected-1>=0){
+                itemInCategory[itemSelected].style.border=" 5px solid rgb(174,90,33)";
+                categorySelected--;
+                itemInCategory=category[categorySelected].getElementsByClassName("item");
+                itemSelected=0;
+                itemInCategory[itemSelected].style.border=" 5px solid white";
+            }
+            
+            break;
+        case 39: // right
+            if(itemSelected+1<nbItemInCategory && toggleSetting==false){
+                itemInCategory[itemSelected].style.border=" 5px solid rgb(174,90,33)";
+                itemSelected++;
+                itemInCategory[itemSelected].style.border=" 5px solid white";;
+            }
+            break;
+        case 40: // down
+                if(toggleSetting==true){
+                    document.getElementById("itemSetting").style.border=" 5px solid rgb(174,90,33)";
+                    toggleSetting=false;
+                }
+                if (categorySelected+1<nbCategoryTotal){
+                        itemInCategory[itemSelected].style.border=" 5px solid rgb(174,90,33)";
+                        categorySelected++;
+                        itemInCategory=category[categorySelected].getElementsByClassName("item");
+                        itemSelected=0;
+                        itemInCategory[itemSelected].style.border=" 5px solid white";
+                    }   
+            break;
+        case 13:
+            break;
+        case 8:
+            break;
+    };
+    console.log("category = "+categorySelected+"; item ="+itemSelected);
+}    

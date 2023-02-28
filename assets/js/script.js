8e1ae054be96da0a045f0280dac6e8b27335f475
Tsouh <erwandvd@gmail.com>
2023-02-28 15:24:05 +0100

    videoMiniDebugTV1

--- assets/js/script.js
+++ assets/js/script.js
@@ -331,14 +331,10 @@ onkeydown = function(evt){
     // console.log(item);
     // console.log(document.activeElement);
 }
-var test2=document.getElementById("video1").getElementsByTagName("video")[0]
-test2.muted=true;
-test2.autoplay=true;
-test2.loop=true;
-// var test=document.getElementById("video1").getElementsByTagName("source")[0];
-// console.log(test);
-// test.setAttribute("src","./assets/img/210530_clip_radio-star_12s.mp4");
-// test.setAttribute("type","video/mp4");
+var test=document.getElementById("video1").getElementsByTagName("video")[0]
+test.muted=true;
+test.autoplay=true;
+test.loop=true;
 
 
 function updateBG(categorySelected,itemSelected){

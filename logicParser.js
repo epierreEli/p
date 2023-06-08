64d98db66c50c2a4ad57b2a824bdbd5b2dc7259f
Quentin HENRY <henryq.dev@outlook.com>
2023-06-08 09:29:39 +0200

    vignette graphic

--- logicParser.js
+++ logicParser.js
@@ -5,10 +5,10 @@ var data = {
             type: "extended",
             children: [
                 {
-                    icon:"icon1.jpg",
+                    icon: "icon1.jpg",
                     title: "titre 1",
                     text: "lorem ipsum, text de description",
-                }, 
+                },
                 {
                     icon: "icon2.jpg",
                     title: "titre 2",
@@ -28,13 +28,13 @@ var data = {
                 {
                     icon: "icon3.jpg",
                     title: "titre 3",
-                    actionType :"link",
+                    actionType: "link",
                     action: "https://www.google.fr",
                 },
                 {
                     icon: "icon3.jpg",
                     title: "titre 3",
-                    actionType :"process",
+                    actionType: "process",
                     action: "process1",
                 },
             ],
@@ -47,10 +47,100 @@ var data = {
                     icon: "icon1.jpg",
                     title: "titre 1",
                     text: "lorem ipsum, text de description",
-                }, 
+                },
             ],
         },
     ]
+}
+
+class Child {
+    constructor({ selectionnable = false, clickable = false, disabled = false }) {
+        this.selectionnable = selectionnable;
+        this.clickable = clickable;
+        this.disabled = disabled;
+    }
+
+    render() { }
+}
+
+class Link extends Child {
+    constructor(title, url) {
+        super({ selectionnable: true, clickable: true });
+        this.title = title;
+        this.url = url;
+    }
+
+    render() {
+        var el = document.createElement("a");
+        el.innerText = this.title;
+        el.href = this.url;
+        return el;
+    }
+}
+
+class Image extends Child {
+    constructor(title, url,width=150,height=100) {
+        super({ selectionnable: true });
+        this.title = title;
+        this.url = url;
+        this.width = width;
+        this.height = height;
+    }
+    render(width=this.width,height=this.height) {
+        var el = document.createElement("img");
+        el.src = this.url;
+        el.alt = this.title;
+        el.width = width;
+        el.height = height;
+        return el;
+    }
+}
+
+export class Vignette extends Image {
+    constructor(title, url) {
+        super(title, url);
+    }
+    render() {     
+        var container = document.createElement("div");
+        container.classList.add("vignette");
+        container.style.height = this.height + "px";
+        container.style.width = this.width + "px";
+        container.style.margin = "10px";
+
+        var image = super.render();
+
+        var title = document.createElement("p");
+        title.innerText = this.title;
+
+        container.appendChild(image);
+        container.appendChild(title);
+
+        return container;
+    }
+}
+
+
+
+export function test() {
+    var testList =[];
+
+    var link = new Link("Lien vers Google", "https://www.google.fr");
+    testList.push(link);
+
+    var image = new Image("Image", "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg");
+    testList.push(image);
+
+    var vignette = new Vignette("Vignette test", "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg");
+    testList.push(vignette);
+
+
+    var container = document.createElement("div");
+    container.style.display = "flex";
+    testList.forEach(element => {
+        container.appendChild(element.render());
+    });
+   
+    return container;
 }
 
 
